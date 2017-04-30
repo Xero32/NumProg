45ce1e7a90c13f2e42ac7ca704654869dea995be
Xero32 <becker.12@web.de>
2017-04-30 19:29:34 +0200

    Add files via upload
    program seems to work;
    first problem leads to big error values, might be due to numerics/rounding/... possibly the purpose of this task;
    second problem returns error value around 1.0E-01 (see comment above invert function);
    routine might be taking too long

--- p2_blaslr.c
+++ p2_blaslr.c
@@ -29,10 +29,10 @@ lrdecomp(pmatrix a){
     int k;
     for(k = 0; k < lda; k++){
         assert(aa[k+k*lda]);
-        scal(lda-k-1, 1.0/aa[k+k*lda], aa+k+1+k*lda, 1);
+        scal(lda-k-1, 1.0/aa[k+k*lda], a->a+k+1+k*lda, 1);
         ger(lda-k-1, lda-k-1, -1.0,
-            aa+(k+1)+k*lda, 1, aa+k+(k+1)*lda, lda,
-            aa+(k+1)+(k+1)*lda, lda);
+            a->a+(k+1)+k*lda, 1, a->a+k+(k+1)*lda, lda,
+            a->a+(k+1)+(k+1)*lda, lda);
     }
 }
 
@@ -41,11 +41,11 @@ static void //TODO
 lowersolve_matrix(int unit, const pmatrix a, pvector b){
     int k;
     int lda = a->ld;
-    double *aa = a->a;
+    //double *aa = a->a;
     double *bb = b->x;
     
     for(k=0; k<unit; k++){
-        axpy(unit-k-1, -bb[k], aa+(k+1)+k*lda, 1, bb+k+1, 1);
+        axpy(unit-k-1, -bb[k], a->a+(k+1)+k*lda, 1, b->x+k+1, 1);
     }
 }
 
@@ -57,15 +57,25 @@ uppersolve_matrix(int unit, const pmatrix a, pvector b){
     double *aa = a->a;
     double *bb = b->x;
     for(k=unit; k-->0;){
-        assert(aa[k+k*lda]);
+        //assert(aa[k+k*lda]);
         bb[k] /= aa[k+k*lda];
-        axpy(k, -bb[k], aa+k*lda, 1, bb, 1);
+        axpy(k, -bb[k], a->a+k*lda, 1, b->x, 1);
     }
     
 }
+/* Problem: Inversion of Upper Matrix not working properly
+ * result is off by only a little 
+ * only last column is off by much (for n=6)
+ * for n=5 last column seems ok
+ * 
+ * 
+ * for any n:   first iteration (Hilbert Matrix) returns a big error value (in the range of 1.0E+n ); for n >> 20 the error is ~1.0E+20
+ *              second iteration (DiagHilbert Matrix) returns error values usually around 1.0E-01, even for big n
+ *  
+ * note: routine might be taking too long
+ * */
 
-
-static pmatrix //TODO
+static pmatrix //TODO  inversion of upper right matrix not working
 invert(const pmatrix a){
     int n = a->rows;
     int lda = a->ld;
@@ -75,8 +85,8 @@ invert(const pmatrix a){
     double *zz = z->a;
     
     
-    for(int j = n-1; j--> 0 ;){
-        for(int i = n; i--> 0;){
+    for(int j = n-1; j--> 0;){
+        for(int i = n; i--> j+1;){
             zz[i+j*lda] = aa[i+j*lda];  //z = lower matrix
             aa[i+j*lda] = 0.0;
         }
@@ -84,8 +94,8 @@ invert(const pmatrix a){
     for(int k = 0; k < n; k++){
         zz[k+k*lda] = 1.0;
     }
-    
-    for(int i = 0; i < n; i++){
+    //TODO
+    for(int i = n; i-- > 0;){
         pvector e = new_zero_vector(n);
         double *ee = e->x;
         ee[i] = 1.0;
@@ -106,13 +116,15 @@ invert(const pmatrix a){
         }
         del_vector(f);
     }
-    for (int j = n-1; j--> 0;){
-        for(int i = n; i--> 0;){
+    
+   for(int j = n-1; j--> 0;){
+        for(int i = n; i--> j+1;){
             aa[i+j*lda] = zz[i+j*lda];
         }
     }
+    del_matrix(z);
         
-    return z;
+    return a;
 }
 
 /* ============================================================
@@ -128,7 +140,7 @@ main(void){
   int rows;
   int i;
   
-  rows = 12;
+  rows = 10;
   
   /* ------------------------------------------------------------
    * Hilbert matrix, LR decomposition
@@ -146,15 +158,16 @@ main(void){
   }
 
   gemv(false, a->rows, a->cols, 1.0, a->a, a->ld, xrev->x, 1, b->x, 1);
-  printf("matrix a\n");
-  print_matrix(a); //TODO
+  //printf("matrix a:\n");
+  //print_matrix(a);
   lrdecomp(a);
-  printf("decomp matrix a\n");
-  print_matrix(a); //TODO
+  //printf("decomp a:\n");
+  //print_matrix(a);
   inva = invert(a);
+  //printf("invert a:\n");
+  //print_matrix(inva);
   gemv(false, a->rows, a->cols, 1.0, inva->a, a->ld, b->x, 1, x->x, 1);
-  printf("inverted matrix\n");
-  print_matrix(inva); //TODO
+
   err_abs = 0.0;
   norm_x  = 0.0;
   for(i = 0; i < rows; i++) {
@@ -186,14 +199,16 @@ main(void){
   for(i = 0; i < rows; i++){
     xrev->x[i] = 1.0 / (1.0 + i);
   }
-
+  //printf("Matrix a:\n");
+  //print_matrix(a);
   gemv(false, a->rows, a->cols, 1.0, a->a, a->ld, xrev->x, 1, b->x, 1);
-  printf("matrix a\n");
-  print_matrix(a);
+  
   lrdecomp(a);
-  printf("decomp matrix a\n");
-  print_matrix(a);
+  //printf("decomp matrix a\n");
+  //print_matrix(a);
   inva = invert(a);
+  //printf("inverted matrix a:\n");
+  //print_matrix(inva);
   gemv(false, a->rows, a->cols, 1.0, inva->a, a->ld, b->x, 1, x->x, 1);
   
   err_abs = 0.0;
@@ -206,8 +221,6 @@ main(void){
   printf("  Absolute max error: %.3e\n", err_abs);
   printf("  Relative max error: %.3e\n", err_abs/norm_x);
     
-  //lowersolve_matrix(rows,a,b);
-  //uppersolve_matrix(rows,a,b);
   del_matrix(a);
   //del_matrix(inva); //TODO
   del_vector(x);
