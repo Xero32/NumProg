aaa610f66bb200330bf8a0eff917350987a5bdcc
Xero32 <becker.12@web.de>
2017-06-05 18:28:46 +0200

    Add files via upload

--- p6_triangulation.c
+++ p6_triangulation.c
@@ -2,8 +2,8 @@
 /*		     Numerische Programmierung 	 	 	 */
 /* 	Serie 6 - Visualisierung von Oberflaechentriangulationen */
 /* ------------------------------------------------------------- */
-/*	Autoren: 				 		 */
-/*	Versionsnummer:						 */
+/*	Autoren: 	Marko Hollm, Marvin Becker 		 */
+/*	Versionsnummer:  1                                       */
 /*---------------------------------------------------------------*/
 
 		
@@ -14,7 +14,7 @@
 #include <GL/glut.h>
   
 #include "surface3d.h"	
-#define RDSP glutPostRedisplay();
+
 #ifndef M_PI
 /* Copied from math.h  */
 #define M_PI		3.14159265358979323846
@@ -25,13 +25,12 @@
 /* ------------------------------------------------------------
 * Global variable
 *-------------------------------------------------------------*/
-							
-psurface3d sur0;//sur1; 
-real x0 = -0.0, y0 = 0.0, z0 = -0.0; //coordinates of mesh
-real xx = 0.0, yy = 0.0, zz = -5.0;
-// real xtr,ytr,ztr; // x-translation, y-translation, z-translation aka zoom
+		
+#define RDSP glutPostRedisplay();
+psurface3d sur0;
+real x0 = 0.0, y0 = 0.0, z0 = 0.0; // translation coordinates
+real xx = 0.0, yy = 1.0, zz = -12.0; // coordinates of body
 real rx,ry, anglex, angley;
-int k = 0;
 int currentx, currenty;
 int Height, Width;
 int rotateflag;
@@ -42,7 +41,8 @@ double zoom = 1.0;
 static void
 Printhelp(){
     printf("\nUsage Info:\n");
-    printf("After inputfile, denote simulation mode:\n\t'm': mesh\n\t'b': solid\n\n");
+    printf("After inputfile, you can denote simulation mode:\n\t'm': mesh\t'b': solid\n");
+    printf("Switch between solid and mesh simulation mode by pressing 'm'\n\n");
     printf("For easier use, the local coordinate system is shown. \n");
     printf("Here the red axis corresponds to the x-axis, the green one to the y-axis \nand the blue one to the z-axis.\n");
     printf("Use Keyboard to move, zoom, and rotate objects.\n\n");
@@ -58,32 +58,22 @@ Printhelp(){
 
 static void
 translate(double x, double y, double z){
-//     glLoadIdentity();
-//     glPushMatrix();
-    
     double tr[16] = {1,0,0,0, 0,1,0,0, 0,0,1,0, x,y,z,1};
     glMultMatrixd(tr);
-//     glutPostRedisplay();
 }
 
 /* Rotation around x-axis */
 static void
 rotate_x(double alpha){
-//     glLoadIdentity();
-//     glPushMatrix();
     double rotx[16] = {1,0,0,0, 0,cos(alpha),sin(alpha),0, 0,-sin(alpha),cos(alpha),0, 0,0,0,1};
-    glMultMatrixd(rotx);
-//     glutPostRedisplay();    
+    glMultMatrixd(rotx);    
 }
 
 /* Rotation around y-axis */
 static void
 rotate_y(double alpha){
-//     glLoadIdentity();
-//     glPushMatrix();
     double roty[16] = {cos(alpha),0,-sin(alpha),0, 0,1,0,0, sin(alpha),0,cos(alpha),0, 0,0,0,1};
     glMultMatrixd(roty);
-//     glutPostRedisplay();
 }
 
 
@@ -91,57 +81,64 @@ rotate_y(double alpha){
  /* Drawing complete surface triangulation */
 static void
 display_mesh(){
+    // prepare window
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
     glEnable(GL_DEPTH_TEST);
-    printf("%f, %f, %f\t\t %f, %f\n",xx,yy,zz,rx,ry);
-    printf("%f, %f, %f\t\t %f, %f\n",x0,y0,z0,anglex,angley);
-    printf("%f\n",zoom);
-    
+    glEnable(GL_NORMALIZE);
     glClearColor(0.9,0.9,0.9,1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
-    glMatrixMode(GL_PROJECTION);
     glPushMatrix();
-    glScalef(zoom,zoom,zoom);
-    if(rotateflag){
-        rotate_y(rx);
-        rotate_x(ry);
-    }else{
-        rotate_x(rx);
-        rotate_y(ry);
-    }
-    translate(x0,y0,z0);
-    glPopMatrix();
-    printf("counter: %d\n",k++);
+    // light
+    glEnable(GL_LIGHTING); 
+    glEnable(GL_LIGHT0);
+    float light_position[4] = { 2.0, -3.0, 4.0, 0.5 }; 
+    float light_ambient[4] = { 0.7, 0.7, 0.7, 0.8 }; 
+    float light_diffuse[4] = { 0.8, 0.8, 0.8, 0.7 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position); 
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient); 
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse); 
+    glEnable(GL_LIGHT1);
+    float light_position1[4] = {1.0, -1.0, -4.0, 1.0};
+    float light_ambient1[4] = { 0.1, 0.1, 0.1, 0.8 }; 
+    float light_diffuse1[4] = { 0.5, 0.5, 0.5, 0.7 };
+    glLightfv(GL_LIGHT1, GL_POSITION, light_position1); 
+    glLightfv(GL_LIGHT1, GL_AMBIENT, light_ambient1); 
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light_diffuse1); 
     
-
+    // scaling
+    if(Width > Height){								
+        glScalef((double) zoom*Height/Width, zoom, zoom);
+    }else{												
+        glScalef(zoom, (double) zoom*Width/Height, zoom);
+    }
+    glPopMatrix();
 
     // draw body
-    glLineWidth(1.0);
-    glColor3f(0.3,0.2,0.4);
-    if(meshflag == 'm'){
-        glBegin(GL_LINES);
-
+    if(meshflag == 'm'){ /* mesh */
+        glColor3f(0.3,0.2,0.4);
+        glLineWidth(1.3);
         int edges = sur0->edges;
         int (*e)[2] = sur0->e;
         real (*x)[3] = sur0->x;
-        
+        glDisable(GL_LIGHT0);
+        glDisable(GL_LIGHT1);
+        glDisable(GL_LIGHTING);
+        glBegin(GL_LINES);
+
         for(int i = 0; i < edges; i++){
             for(int j = 0; j < 2; j++){
                 glVertex3f(x[e[i][j]][0], x[e[i][j]][1], x[e[i][j]][2]);
             }
         }
-        
-    }else if(meshflag == 'b'){
+    }else if(meshflag == 'b'){ /* solid */
         int triangles=sur0->triangles;
 	int (*t)[3]=sur0->t;
 	real (*x)[3]=sur0->x;
 	real (*n)[3]=sur0->n;
         glBegin(GL_TRIANGLES);
-// 	glMatrixMode(GL_MODELVIEW);
-//         glPushMatrix();
-	float material_ambient[4] = { 1.0, 0.0, 0.0, 0.0 }; 
-	float material_diffuse[4] = { 1.0, 0.0, 0.0, 0.0 };
+	float material_ambient[4] = { 0.3, 0.2, 0.4, 0.0 }; 
+	float material_diffuse[4] = { 0.6, 0.4, 0.8, 0.0 };
 	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, material_ambient); 
 	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, material_diffuse);
 
@@ -156,6 +153,9 @@ display_mesh(){
     glEnd();
     
     // draw coordinate system
+    glDisable(GL_LIGHT0);
+    glDisable(GL_LIGHT1);
+    glDisable(GL_LIGHTING);
     glLineWidth(1.5);
     glBegin(GL_LINES);
     glColor3f(1.0,0.0,0.0);
@@ -177,7 +177,6 @@ display_mesh(){
   /* Reshape of a triangulation */
 static void
 reshape_mesh(int width, int height){
-GLfloat p[4],a[4],d[4];
     Width = width;
     Height = height;
     glViewport(0, 0, width, height); 
@@ -190,50 +189,21 @@ GLfloat p[4],a[4],d[4];
         else{												
         glScalef(1.0, (double) width/height, 1.0);
         }
-    /*
-    if(width > height)
-        glViewport((width-height)/2, 0, height, height);
-    else
-        glViewport(0, (height-width)/2, width, width);
-    */
-    printf("RESHAPE\n");
+
     gluPerspective(35.0, 1.0, 1.0, 30.0);
     glTranslatef(xx,yy,zz);
-    rotate_y(angley);
-    rotate_x(anglex);
-    
-//     glRotatef(anglex,1.0,0.0,0.0);
-//     glRotatef(angley,0.0,1.0,0.0);
-//     translate(x0,y0,z0);
+
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();	
-
-    glEnable(GL_LIGHTING);	
-
-    glEnable(GL_LIGHT0);
-
-    p[0] = -2.0; p[1] = 2.0; p[2] = 2.0; p[3] = 1.0;
-    glLightfv(GL_LIGHT0, GL_POSITION, p); 	
-
-    a[0] = 0.6; a[1] = 0.6; a[2] = 0.6; a[3] = 1.0;	
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  a);
-
-    d[0] = 1.0; d[1] = 1.0; d[2] = 1.0; d[3] = 1.0;
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, d); 
-
-    glEnable(GL_COLOR_MATERIAL);   					
-    glMaterialfv(GL_FRONT, GL_AMBIENT, a);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE, d);
-
+    glPopMatrix();
     glEnable(GL_DEPTH_TEST);
 }
 
   /* Mouse movement on a triangulation */
-
-  static void
+static void
 mouse_mesh(int button, int state, int position_y, int position_x){
     currentx = position_x;
     currenty = position_y;
+    // mouse wheel functions
     if(button == 3){
         glScalef(1.01,1.01,1.01); 
         RDSP;
@@ -246,16 +216,16 @@ mouse_mesh(int button, int state, int position_y, int position_x){
   /* Motion on a triangulation */
 static void
 motion_mesh(int position_y, int position_x){
+    // mouse motion for rotation
     rotate_x(rx=(float)(position_x-currentx)/Height);
     anglex+=rx;
     rotate_y(ry=(float)(position_y-currenty)/Height);
     angley+=ry;
     currenty=position_y;
     currentx=position_x;
-    glutPostRedisplay();
+    RDSP;
 }
 
- 
   /* Key input for triangulation */
 static void
 key_mesh(unsigned char key, int x, int y){
@@ -269,28 +239,28 @@ key_mesh(unsigned char key, int x, int y){
         case 'y': translate(0.0,0.0,z0=-0.05); zz+=z0; RDSP; break; 
         case 0x02B: glScalef(1.01,1.01,1.01); zoom+=0.01; RDSP; break; //'x' key
         case 0x02D: glScalef(0.99,0.99,0.99); zoom-=0.01; RDSP; break; // '-' key
-        case 'r': rotate_x(rx=M_PI*0.01); anglex+=rx; rotateflag = 0; RDSP; break;
-        case 'f': rotate_x(rx=-M_PI*0.01); anglex+=rx; rotateflag = 0; RDSP; break;
-        case 'q': rotate_y(ry=M_PI*0.01); angley+=ry; rotateflag = 1; RDSP; break;
-        case 'e': rotate_y(ry=-M_PI*0.01); angley+=ry; rotateflag = 1; RDSP; break;
+        case 'r': rotate_x(rx=M_PI*0.01); anglex+=rx; RDSP; break;
+        case 'f': rotate_x(rx=-M_PI*0.01); anglex+=rx; RDSP; break;
+        case 'q': rotate_y(ry=M_PI*0.01); angley+=ry; RDSP; break;
+        case 'e': rotate_y(ry=-M_PI*0.01); angley+=ry; RDSP; break;
         case 'h': Printhelp(); break;
-        case 'c': glLoadIdentity(); anglex=angley=0.0; xx=yy=zz=0.0; RDSP; break;
-
-        case 27: exit(EXIT_SUCCESS);
+        case 'c': glLoadIdentity(); RDSP; break;
+        case 'm': meshflag = (meshflag == 'b') ? 'm' : 'b'; RDSP; break;
+        case 0x01B: exit(EXIT_SUCCESS); // 'esc'-key
     }
 }
 
-  
 /* last but not least the main function */   
 int
 main(int argc,char **argv){
 //   psurface3d sur;
-//   int i;
+//   int i;             // no idea, what these variables are meant to do
   
   /* Reading mesh */
   if(argc > 1){
     sur0 = read_surface3d(argv[1]); 
-    if(argc > 2) meshflag = argv[2][0];
+    if(argc > 2) meshflag = argv[2][0]; //rudimentary input function to denote, whether to draw mesh or solid
+    else meshflag = 'b'; // set solid as standard case
 	}
   else{
     printf("No input file!\n");
@@ -301,22 +271,19 @@ main(int argc,char **argv){
   }else{
       printf("For help press 'h'\n");
   }
-	glutInit(&argc, argv);
-	glutCreateWindow("Triangulation (for help press 'h')");
-	glutPositionWindow(450, 400);
-	glutReshapeWindow(800, 800);
+    glutInit(&argc, argv);
+    glutCreateWindow("Triangulation (for help press 'h')");
+    glutPositionWindow(450, 400);
+    glutReshapeWindow(800, 800);
 	
-	glutReshapeFunc(reshape_mesh);
-	glutDisplayFunc(display_mesh);
+    glutReshapeFunc(reshape_mesh);
+    glutDisplayFunc(display_mesh);
 
-	glutMouseFunc(mouse_mesh);
+    glutMouseFunc(mouse_mesh);
 
     glutMotionFunc(motion_mesh);
 
     glutKeyboardFunc(key_mesh);
-
-//     glutTimerFunc(50, timer, 0);
-	
-	glutMainLoop();
+    glutMainLoop();
   return EXIT_SUCCESS;
 }
